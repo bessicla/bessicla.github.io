2c2bfedea4294147b792a5b80ed999b3bffe7337
Claudia <bessicla@gmail.com>
2020-02-13 11:49:57 +0100

    new leaf

--- shuffler.js
+++ shuffler.js
@@ -1,8 +1,28 @@
-$(document).ready(function () {
-    var cards = $(".masonry");
-    for (var i = 0; i < cards.length; i++) {
-        var target = Math.floor(Math.random() * cards.length - 1) + 1;
-        var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
-        cards.eq(target).before(cards.eq(target2));
-    }
-});
+(function($){
+ 
+    $.fn.shuffle = function() {
+ 
+        var allElems = this.get(),
+            getRandom = function(max) {
+                return Math.floor(Math.random() * max);
+            },
+            shuffled = $.map(allElems, function(){
+                var random = getRandom(allElems.length),
+                    randEl = $(allElems[random]).clone(true)[0];
+                allElems.splice(random, 1);
+                return randEl;
+           });
+ 
+        this.each(function(i){
+            $(this).replaceWith($(shuffled[i]));
+        });
+ 
+        return $(shuffled);
+ 
+    };
+ 
+})(jQuery);
+
+ $(document).ready(function() {
+       $(".item").shuffle();
+ });
