b723ab369ab7e9d6cc50eaf13b96f2230dc43030
Claudia <bessicla@gmail.com>
2020-05-01 18:41:55 +0200

    Update shuffler.js

--- shuffler.js
+++ shuffler.js
@@ -33,4 +33,28 @@ $(window).ready(function () {
   }, 400);
 });
 
+$(function() {  
+
+      // To keep track of how many images have loaded
+    var loaded = 0;
+
+      // Let's retrieve how many images there are
+    var numImages = $("img").length;
+
+      // Let's bind a function to the loading of EACH image
+    $("img").load(function() {
+
+          // One more image has loaded
+        ++loaded;
+
+          // Only if ALL the images have loaded
+        if (loaded === numImages) {
+
+              $( "#grid" ).animate({
+    opacity: 1,
+  }, 400);
+         
+        }
+    });
+});
 
